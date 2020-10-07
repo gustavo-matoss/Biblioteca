b682d09dc1ea15d9405b453981c4e3dec743689d
gustavo-matoss <69210690+gustavo-matoss@users.noreply.github.com>
2020-10-07 15:21:25 -0300

    Add files via upload

---
+++ Biblioteca.ListaLeitura/inicializadb.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE biblioteca_teste
+
+CREATE TABLE biblioteca(
+  ID samllint NOT NULL DEFAULT '1899',
+  TITULO varchar(64),
+  AUTOR varchar(64),
+  ALUGADO smallint  NOT NULL DEFAULT '0',
+  CONSTRAINT biblioteca PRIMARY KEY  (ID,TITULO,AUTOR)
+);
+
+CREATE TABLE users(
+     ID samllint NOT NULL DEFAULT '1899',
+     USER varchar(64),
+     PASSWORD varchar(64),
+	 CONSTRAINT users PRIMARY KEY  (ID,USER)
+);
